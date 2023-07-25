48e6a27b162b3156a1e2d90685c5cca65bd42205
David Massey <dapmassey@gmail.com>
2023-07-25 15:28:26 +0100

    addition of smile detection

--- facedetector.py
+++ facedetector.py
@@ -1,11 +1,14 @@
 import cv2
+import numpy
 
-# Step 1 Setting up the Face Detector
+# Setting up the Face and Smile Detector
 
-# load some pre trained data on face data from https://github.com/opencv/opencv/tree/4.x/data/haarcascades
+## load some pre trained data on face data from https://github.com/opencv/opencv/tree/4.x/data/haarcascades
 face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+## load some pre trained data on face data from https://github.com/opencv/opencv/blob/4.x/data/haarcascades/haarcascade_smile.xml
+smile = cv2.CascadeClassifier('haarcascade_smile.xml')
 
-## This is Static Picture Code
+### This is Static Picture Code
 
 '''
 # image to detect faces in
@@ -34,39 +37,63 @@ cv2.imshow('Clever Programmer Face Detector', img)
 cv2.waitKey()
 '''
 
-## This is Webcam code
+### This is Webcam code
 
 # To capture video from webcam
-webcam = cv2.VideoCapture(0) # one face defaulted to webacam. Add a video path instead.
+webcam = cv2.VideoCapture(0) # one face defaulted to webcam. Add a video path instead.
 
 # Iterate forever over frames
 while True:
-    # read the current frame
+    # read the current frame from the webcam video stream
     successful_frame_read, frame = webcam.read()
     
-    # convert to greyscale
+    # if theres and error, abort
+    if not successful_frame_read:
+        break
+    
+    # convert to image to greyscale as this is easier to calculate, therefore less processing.
     grayscaled_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     
     # detect faces
-    face_coordinates = face.detectMultiScale(grayscaled_img)
-
-    # draw a green rectangle around face dynamically
-    for (x, y, w, h) in face_coordinates:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2 )
-
+    faces = face.detectMultiScale(grayscaled_img)
+    
+    # run face detection within each of the faces
+    for (x, y, w, h) in faces:
+        
+        # draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (100,200,50), 4 )
+        
+        # get the sub frame (using numpy N-dimensional array slicing)
+        the_face = frame[y:y+h, x:x+w]
+        
+        # change to greyscale
+        face_grayscale = cv2.cvtColor(the_face,cv2.COLOR_BGR2GRAY)
+        
+        smiles = smile.detectMultiScale(face_grayscale,scaleFactor=1.7, minNeighbors=20)
+        
+        # find all smiles in the face
+        for (x_, y_, w_, h_) in smiles:
+            
+            # draw rectangles around the smile
+            #cv2.rectangle(the_face, (x_, y_), (x_+w_, y_+h_), (0,255,0), 2)
+            
+        # label this face as smiling
+            if len(smiles) > 0:
+                cv2.putText(frame, 'Smiling', (x, y+h+40), fontScale=3, fontFace=cv2.FONT_HERSHEY_PLAIN, color=(255, 255, 255))
+        
     # this will show the image
-    cv2.imshow('bobdatcod.e Face Detector', frame)
+    cv2.imshow('bobdatcod.e Face Booth', frame)
+    
+    # Display
     key = cv2.waitKey(1)
     
-    # Stop if 0 key is pressed
+    # Stop if Q key is pressed
     ''' 81 is the ascii code for q'''
     if key == 81  or key == 113:
         break
 
 # Release the VideoCapture object
 webcam.release()
-
-# Step 2 
-
+cv2.destroyAllWindows()
 
 print('Code Completed')
